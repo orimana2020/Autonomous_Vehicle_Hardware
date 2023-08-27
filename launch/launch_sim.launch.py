2522d981b9f87c246daff18c362f44e1c09b58ff
Ori <ori.mana@gmail.com>
2023-08-27 12:19:24 +0300

    odom works!

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -1,36 +1,25 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
-
-
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
 from launch_ros.actions import Node
 
-
-
 def generate_launch_description():
-
-
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
     # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
     package_name='rc_car' #<--- CHANGE ME
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
-    )
-
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items())
+    
     joystick = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','joystick.launch.py'
                 )]), launch_arguments={'use_sim_time': 'true'}.items())
         
-
     twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
     twist_mux = Node(
             package="twist_mux",
@@ -62,17 +51,20 @@ def generate_launch_description():
                                    
                         
 
-
     diff_drive_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["diff_cont"],
+        output="screen",
+        # remappings=[('/diff_cont/tf_odometry','/tf')]
     )
 
     joint_broad_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["joint_broad"],
+        output="screen",
+        # remappings=[('/joint_states','/tf'), ('/dynamic_joint_states', '/tf')]
     )
 
     relay_topic_to_tf_node = Node(
@@ -83,7 +75,7 @@ def generate_launch_description():
     )
 
 
-
+    
 
 
     # Launch them all!
@@ -96,5 +88,6 @@ def generate_launch_description():
         spawn_entity,
         diff_drive_spawner,
         joint_broad_spawner,
-        relay_topic_to_tf_node
+        relay_topic_to_tf_node,
+        
     ])
--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -1,10 +1,13 @@
 import os
+
 from ament_index_python.packages import get_package_share_directory
+
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration, Command
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 import xacro
+
 
 
 def generate_launch_description():
@@ -20,35 +23,37 @@ def generate_launch_description():
     robot_description_config = Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control, ' sim_mode:=', use_sim_time])
     
     # Create a robot_state_publisher node
-    # params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
-    params = {'robot_description': robot_description_config}
+    params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
 
-
-    node_joint_state_publisher = Node(
+    node_joint_state_publisher = Node( # not needed when use joint_broad ros2_control
         package='joint_state_publisher',
         executable='joint_state_publisher',
-        name='joint_state_publisher')
+        name='joint_state_publisher',
+        parameters=[params],)
+
 
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
+        name='robot_state_publisher',
         output='screen',
-        parameters=[params] )
-   
-
+        parameters=[params],
+        # remappings=[('/diff_cont/tf_odometry','/tf')]
+          )
    
 
     # Launch!
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
-            default_value='false',
+            default_value='true',
             description='Use sim time if true'),
         DeclareLaunchArgument(
             'use_ros2_control',
             default_value='true',
             description='Use ros2_control if true'),
-
+        
+        # control_node,
         node_joint_state_publisher,
         node_robot_state_publisher,
     ])
