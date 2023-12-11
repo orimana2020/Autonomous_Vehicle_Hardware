b5de39da75a589d8a092272eb1fa2f478b0490ed
ori.m <ori.mana@gmail.com>
2023-12-11 17:13:53 +0200

    calibrate steering angle and backup low level

--- rc_car/scripts/inverse_twist.py
+++ rc_car/scripts/inverse_twist.py
@@ -28,6 +28,7 @@ class InverseTwist(Node):
 
         self.wheel_radius = 0.056
         self.wheelbase = 0.3429
+        # if max_steering_angle changed the value in ROS_ARDUINO_BRIDGE must be changed too!
         self.max_steering_angle = 0.6 #  = 35[deg] * np.pi / 180
         max_linear_velocity = 2
         self.max_radial_velocity_rear_wheel = max_linear_velocity / self.wheel_radius 
